ef26a1ae9ed1b98cc901e5374ef372929963f5c4
MehdiLotfian <mehdilotfian.meloent@gmail.com>
2021-07-17 20:23:26 +0430

    client first connection settings

---
+++ src/main/java/ConnectionController.java
@@ -0,0 +1,33 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ConnectionController {
+
+    private static Socket socket;
+    private static DataInputStream dataInputStream;
+    private static DataOutputStream dataOutputStream;
+
+    public static void setupConnection() {
+        try {
+            socket = new Socket("localhost", 7755);
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public DataInputStream getDataInputStream() {
+        return dataInputStream;
+    }
+
+    public DataOutputStream getDataOutputStream() {
+        return dataOutputStream;
+    }
+}
